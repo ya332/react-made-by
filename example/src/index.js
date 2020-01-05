f513372a79901109230f741e118e9a771920a27a
Yigit Alparslan <ya332@drexel.edu>
2020-01-04 19:07:00 -0500

    Update gitignore files

--- example/src/index.js
+++ example/src/index.js
@@ -1,15 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import MadeBy from '../../src/components/MadeBy';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
 
-const App = React.createClass({
-	render() {
-		return (
-			<div>
-				<MadeBy />
-			</div>
-		);
-	},
-});
+ReactDOM.render(<App />, document.getElementById('root'));
 
-ReactDOM.render(<App />, document.getElementById('example'));
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
