4ab6f202766c1bd40435db5c772fa0e0da2b2ef3
Yigit Alparslan <ya332@drexel.edu>
2020-01-04 21:57:08 -0500

    Add webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -6,32 +6,15 @@ module.exports = {
 		filename: "index.js",
 		libraryTarget: "commonjs2" // THIS IS THE MOST IMPORTANT LINE! :mindblow: I wasted more than 2 days until realize this was the line most important in all this guide.
 	},
-	module: {
-		loaders: [
-			{
-				test: /\.jsx?$/,
-				loader: 'babel',
-				exclude: /node_modules/,
-				query: {
-					cacheDirectory: true,
-					presets: ['react', 'es2015']
-				}
-			}
-		],
-		rules: [
-			{
-				test: /\.js$/,
-				include: path.resolve(__dirname, "src"),
-				exclude: /(node_modules|bower_components|build)/,
-				use: {
-					loader: "babel-loader",
-					options: {
-						presets: ["react, es2015"]
-					}
-				}
-			}
-		]
-	},
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                exclude: /(node_modules)/,
+                use: 'babel-loader'
+            }
+        ]
+    },
 	externals: {  // this line is just to use the React dependency of our parent-testing-project instead of using our own React.
 		react: {
 			root: 'React',
