77d6234fc0a3bd1a9be8ef713d32b0133c4f74a6
Yigit Alparslan <ya332@drexel.edu>
2020-01-04 20:28:17 -0500

    Add default text for the component

--- src/index.js
+++ src/index.js
@@ -57,7 +57,7 @@ const MadeBy = (props) => {
             <div className="MadeBy__div" style={{...fadeStyle, ...defaultMadeByStyles}}{...fadeProps}>
 
                 <img className="MadeBy__img" style={defaultImgStyles} src={props.src ? props.src : ""}></img>
-                <p className="MadeBy__p" style={defaultPStyles}>{props.text}</p>
+                <p className="MadeBy__p" style={defaultPStyles}>{props.text ? props.text : "Made with ☕ by Me!"}</p>
             </div >
         </a>
     )
